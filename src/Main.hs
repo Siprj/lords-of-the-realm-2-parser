6b19d79a876e35d80bc7b5b43b754a3df85a2938
Jan Sipr <sipr.jan@gmail.com>
2019-05-18 12:29:40 +0200

    Performance improvements

--- src/Main.hs
+++ src/Main.hs
@@ -16,6 +16,7 @@ import Control.Monad.ST
 import qualified Control.Monad.Trans.State.Strict as S
 import Control.Monad.Trans.Class
 import Data.ByteString hiding (putStrLn, zipWith, head, maximum, zip, replicate, length, take)
+import qualified Data.ByteString as BS
 import Data.ByteString.Lazy (toStrict)
 import Data.Either
 import Data.Bits
@@ -31,7 +32,8 @@ import Data.Serialize.Get
 import Data.String
 import Data.Traversable
 import Data.Vector hiding (drop, take, maximum, mapM_, mapM, sequence, length)
-import qualified Data.Vector.Mutable as VM
+import qualified Data.Vector.Unboxed as VU
+import qualified Data.Vector.Unboxed.Mutable as VM
 import qualified Data.Vector.Generic as V
 import Data.Word (Word16, Word32, Word8)
 import Debug.Trace
@@ -56,10 +58,11 @@ data Tile a = Tile
     , extraRows :: Int
     , unknowenByte1 :: Word32
     , unknowenByte2 :: Word32
-    , indicesToPallet :: a (Vector Word8)
+    , indicesToPallet :: a (VU.Vector Word8)
     }
 
-tileHeaderToTile :: Tile Proxy -> Vector Word8 -> Tile Identity
+{-# INLINE tileHeaderToTile #-}
+tileHeaderToTile :: Tile Proxy -> VU.Vector Word8 -> Tile Identity
 tileHeaderToTile Tile{..} indices = Tile
     { width
     , height
@@ -73,7 +76,7 @@ tileHeaderToTile Tile{..} indices = Tile
     , indicesToPallet = Identity indices
     }
 
-deriving instance (Show (a (Vector Word8))) => Show (Tile a)
+deriving instance (Show (a (VU.Vector Word8))) => Show (Tile a)
 
 data File = File
     { unknowenByte3 :: Word16
@@ -83,6 +86,7 @@ data File = File
     }
   deriving (Show)
 
+{-# INLINE getPallet #-}
 getPallet :: Get (Vector PixelRGB8)
 getPallet = replicateM 256 getPixel
   where
@@ -91,6 +95,7 @@ getPallet = replicateM 256 getPixel
         <*> fmap (4*) getWord8
         <*> fmap (4*) getWord8
 
+{-# INLINE printFileHeaders #-}
 printFileHeaders :: Handle -> IO ()
 printFileHeaders fHandle = do
     files <- L.sort <$> globDir1 (compile "*.pl8") "/home/yrid/.local/share/Steam/steamapps/common/Lords of the Realm II/English/Lords of the Realm II/"
@@ -115,6 +120,7 @@ printFileHeaders fHandle = do
                 hPrintf fHandle " offset: %d width: %d height: %d\n" offset width height
             else pure ()
 
+{-# INLINE convertFiles #-}
 convertFiles :: IO ()
 convertFiles = do
     files <- L.sort <$> globDir1 (compile "*.pl8") "/home/yrid/.local/share/Steam/steamapps/common/Lords of the Realm II/English/Lords of the Realm II/"
@@ -126,10 +132,12 @@ convertFiles = do
     convert (input, output) =
         convertToRgb input output "/home/yrid/.local/share/Steam/steamapps/common/Lords of the Realm II/English/Lords of the Realm II/Base01.256"
 
+{-# INLINE eitherToError #-}
 eitherToError :: Either String b -> IO b
 eitherToError (Right a) = pure a
 eitherToError (Left e) = fail e
 
+{-# INLINE convertBy #-}
 convertBy
     :: PngSavable a
     => (File -> IO (Image a))
@@ -143,13 +151,11 @@ convertBy f input output = do
     image <- f file
     writeFile output . toStrict $ encodePng image
 
-convertToGray :: FilePath -> FilePath -> IO ()
-convertToGray = convertBy fileToGrayImage
-
+{-# INLINE convertToRgb #-}
 convertToRgb :: FilePath -> FilePath -> FilePath -> IO ()
 convertToRgb input output palletFile = do
-    p <- readPallet
-    convertBy (fileToRGBImage (Just p)) input output
+    pallet <- readPallet
+    convertBy (fileToRGBImage pallet) input output
   where
     readPallet = readFile palletFile >>= (eitherToError . runGet getPallet)
 
@@ -162,12 +168,14 @@ getFile = do
     finalTiles <- mapM (if testBit unknowenByte3' 0 then getRLETile else getTile) tiles'
     pure $ File unknowenByte3' numberOfTiles' unknowenByte4' finalTiles
 
+{-# INLINE getTile #-}
 getTile :: Tile Proxy -> Get (Tile Identity)
 getTile header@Tile{..} = do
     if extraType == 0
        then getSimplTile header
        else getISOTile header
 
+{-# INLINE getISOTile #-}
 getISOTile :: Tile Proxy -> Get (Tile Identity)
 getISOTile header@Tile{..} = do
     data' <- getByteString $ dataLength + (fromIntegral extraRows)
@@ -256,23 +264,22 @@ up x y
         | x' >= y = []
         | P.otherwise = x' : go_up (x'+1)
 
+{-# INLINE getSimplTile #-}
 getSimplTile :: Tile Proxy -> Get (Tile Identity)
 getSimplTile header@Tile{..} = do
-    kwa <- remaining
-    -- let foo = show (height * width) <> " kwa: " <> show kwa <> "\n"
-    -- indices <- getByteString $ trace foo $ min (height * width) kwa
-    indices <- getByteString $ min (height * width) kwa
-    pure $ tileHeaderToTile header . fromList . unpack $ indices
+    tileHeaderToTile header <$> VU.replicateM (height * width) getWord8
 
+{-# INLINE getRLETile #-}
 getRLETile :: Tile Proxy -> Get (Tile Identity)
 getRLETile header@Tile{..} =
     tileHeaderToTile header <$> getRLEData (fromIntegral (height * width))
 
-getRLEData :: Int -> Get (Vector Word8)
+{-# INLINE getRLEData #-}
+getRLEData :: Int -> Get (VU.Vector Word8)
 getRLEData size = go mempty
   where
-    go :: Vector Word8 -> Get (Vector Word8)
-    go pixels = if length pixels < size
+    go :: VU.Vector Word8 -> Get (VU.Vector Word8)
+    go pixels = if VU.length pixels < size
         then readData pixels
         else pure pixels
 
@@ -281,11 +288,12 @@ getRLEData size = go mempty
         if numOfOpaquePixels == 0
             then do
                 numberOfTransparetnPixels <- fromIntegral <$> getWord8
-                go $ pixels <> replicate numberOfTransparetnPixels 0
+                go $ pixels <> VU.replicate numberOfTransparetnPixels 0
             else do
-                data' <- fromList . unpack <$> getByteString numOfOpaquePixels
+                data' <- VU.replicateM numOfOpaquePixels getWord8
                 go $ pixels <> data'
 
+{-# INLINE getTileHeader #-}
 getTileHeader :: Get (Tile Proxy)
 getTileHeader = fmap correctextraRows $ Tile
     <$> fmap fromIntegral getWord16le
@@ -303,303 +311,40 @@ getTileHeader = fmap correctextraRows $ Tile
         { extraRows = if extraType == 1 then 0 else extraRows
         }
 
+{-# INLINE getSize #-}
 getSize :: Vector (Tile a) -> (Int, Int)
 getSize tiles =
     ( maximum $ fmap (\Tile{..} -> x + width) tiles
     , maximum $ fmap (\Tile{..} -> y + height) tiles
     )
 
-fileToGrayImage :: File -> IO (Image Pixel8)
-fileToGrayImage File{..} = do
-    let (width, height) = getSize tiles
-    image <- newMutableImage @Pixel8 width height
-    mapM_ (fillPixel image) . fold $ fmap dataWihtCoordiantes tiles
-    unsafeFreezeImage image
-  where
-    dataWihtCoordiantes :: Tile Identity -> Vector (Word8, (Int, Int))
-    dataWihtCoordiantes Tile{..} =
-        zip (runIdentity indicesToPallet) $ fromList coordinates
-      where
-        coordinates = [(x, y) | y <- [y..y+height], x <- [x..x+width]]
-
-    fillPixel image (pixel, (x, y)) = writePixel image x y pixel
-
-fileToRGBImage :: Maybe (Vector PixelRGB8) -> File -> IO (Image PixelRGB8)
-fileToRGBImage maybePallet File{..} = do
+fileToRGBImage :: Vector PixelRGB8 -> File -> IO (Image PixelRGB8)
+fileToRGBImage pallet File{..} = do
     let (width, height) = getSize tiles
     image <- newMutableImage @PixelRGB8 width height
-    mapM_ (fillPixel image) . fold $ fmap dataWihtCoordiantes tiles
+    mapM_ (fillPixels image) tiles
     unsafeFreezeImage image
   where
-    dataWihtCoordiantes :: Tile Identity -> Vector (Word8, (Int, Int))
-    dataWihtCoordiantes Tile{..} =
-        zip (runIdentity indicesToPallet) $ fromList coordinates
-      where
-        coordinates = [(x, y) | y <- [y..y+height-1], x <- [x..x+width-1]]
+    makeCoordinates :: Tile Identity -> [(Int, Int)]
+    makeCoordinates Tile{..} =
+        [(x, y) | y <- [y..y+height-1], x <- [x..x+width-1]]
 
-    fillPixel image (pixel, (x, y)) = if fromIntegral pixel /= 0
-        then writePixel image x y
-            $ fromMaybe defaultPallet maybePallet ! fromIntegral pixel
-        else pure ()
+    fillPixels :: MutableImage RealWorld PixelRGB8 -> Tile Identity -> IO ()
+    fillPixels image tile@Tile{..} =
+        fillPixels' image (runIdentity indicesToPallet) (makeCoordinates tile)
 
-    defaultPallet = fromList
-        [ PixelRGB8 0 0 0
-        , PixelRGB8 0 0 128
-        , PixelRGB8 0 128 0
-        , PixelRGB8 0 128 128
-        , PixelRGB8 128 0 0
-        , PixelRGB8 128 0 128
-        , PixelRGB8 128 128 0
-        , PixelRGB8 192 192 192
-        , PixelRGB8 192 220 192
-        , PixelRGB8 200 208 212
-        , PixelRGB8 0 32 64
-        , PixelRGB8 0 32 96
-        , PixelRGB8 0 32 128
-        , PixelRGB8 0 32 160
-        , PixelRGB8 0 32 192
-        , PixelRGB8 0 32 224
-        , PixelRGB8 0 64 0
-        , PixelRGB8 0 64 32
-        , PixelRGB8 0 64 64
-        , PixelRGB8 0 64 96
-        , PixelRGB8 0 64 128
-        , PixelRGB8 0 64 160
-        , PixelRGB8 0 64 192
-        , PixelRGB8 0 64 224
-        , PixelRGB8 0 96 0
-        , PixelRGB8 0 96 32
-        , PixelRGB8 0 96 64
-        , PixelRGB8 0 96 96
-        , PixelRGB8 0 96 128
-        , PixelRGB8 0 96 160
-        , PixelRGB8 0 96 192
-        , PixelRGB8 0 96 224
-        , PixelRGB8 0 128 0
-        , PixelRGB8 0 128 32
-        , PixelRGB8 0 128 64
-        , PixelRGB8 0 128 96
-        , PixelRGB8 0 128 128
-        , PixelRGB8 0 128 160
-        , PixelRGB8 0 128 192
-        , PixelRGB8 0 128 224
-        , PixelRGB8 0 160 0
-        , PixelRGB8 0 160 32
-        , PixelRGB8 0 160 64
-        , PixelRGB8 0 160 96
-        , PixelRGB8 0 160 128
-        , PixelRGB8 0 160 160
-        , PixelRGB8 0 160 192
-        , PixelRGB8 0 160 224
-        , PixelRGB8 0 192 0
-        , PixelRGB8 0 192 32
-        , PixelRGB8 0 192 64
-        , PixelRGB8 0 192 96
-        , PixelRGB8 0 192 128
-        , PixelRGB8 0 192 160
-        , PixelRGB8 0 192 192
-        , PixelRGB8 0 192 224
-        , PixelRGB8 0 224 0
-        , PixelRGB8 0 224 32
-        , PixelRGB8 0 224 64
-        , PixelRGB8 0 224 96
-        , PixelRGB8 0 224 128
-        , PixelRGB8 0 224 160
-        , PixelRGB8 0 224 192
-        , PixelRGB8 0 224 224
-        , PixelRGB8 64 0 0
-        , PixelRGB8 64 0 32
-        , PixelRGB8 64 0 64
-        , PixelRGB8 64 0 96
-        , PixelRGB8 64 0 128
-        , PixelRGB8 64 0 160
-        , PixelRGB8 64 0 192
-        , PixelRGB8 64 0 224
-        , PixelRGB8 64 32 0
-        , PixelRGB8 64 32 32
-        , PixelRGB8 64 32 64
-        , PixelRGB8 64 32 96
-        , PixelRGB8 64 32 128
-        , PixelRGB8 64 32 160
-        , PixelRGB8 64 32 192
-        , PixelRGB8 64 32 224
-        , PixelRGB8 64 64 0
-        , PixelRGB8 64 64 32
-        , PixelRGB8 64 64 64
-        , PixelRGB8 64 64 96
-        , PixelRGB8 64 64 128
-        , PixelRGB8 64 64 160
-        , PixelRGB8 64 64 192
-        , PixelRGB8 64 64 224
-        , PixelRGB8 64 96 0
-        , PixelRGB8 64 96 32
-        , PixelRGB8 64 96 64
-        , PixelRGB8 64 96 96
-        , PixelRGB8 64 96 128
-        , PixelRGB8 64 96 160
-        , PixelRGB8 64 96 192
-        , PixelRGB8 64 96 224
-        , PixelRGB8 64 128 0
-        , PixelRGB8 64 128 32
-        , PixelRGB8 64 128 64
-        , PixelRGB8 64 128 96
-        , PixelRGB8 64 128 128
-        , PixelRGB8 64 128 160
-        , PixelRGB8 64 128 192
-        , PixelRGB8 64 128 224
-        , PixelRGB8 64 160 0
-        , PixelRGB8 64 160 32
-        , PixelRGB8 64 160 64
-        , PixelRGB8 64 160 96
-        , PixelRGB8 64 160 128
-        , PixelRGB8 64 160 160
-        , PixelRGB8 64 160 192
-        , PixelRGB8 64 160 224
-        , PixelRGB8 64 192 0
-        , PixelRGB8 64 192 32
-        , PixelRGB8 64 192 64
-        , PixelRGB8 64 192 96
-        , PixelRGB8 64 192 128
-        , PixelRGB8 64 192 160
-        , PixelRGB8 64 192 192
-        , PixelRGB8 64 192 224
-        , PixelRGB8 64 224 0
-        , PixelRGB8 64 224 32
-        , PixelRGB8 64 224 64
-        , PixelRGB8 64 224 96
-        , PixelRGB8 64 224 128
-        , PixelRGB8 64 224 160
-        , PixelRGB8 64 224 192
-        , PixelRGB8 64 224 224
-        , PixelRGB8 128 0 0
-        , PixelRGB8 128 0 32
-        , PixelRGB8 128 0 64
-        , PixelRGB8 128 0 96
-        , PixelRGB8 128 0 128
-        , PixelRGB8 128 0 160
-        , PixelRGB8 128 0 192
-        , PixelRGB8 128 0 224
-        , PixelRGB8 128 32 0
-        , PixelRGB8 128 32 32
-        , PixelRGB8 128 32 64
-        , PixelRGB8 128 32 96
-        , PixelRGB8 128 32 128
-        , PixelRGB8 128 32 160
-        , PixelRGB8 128 32 192
-        , PixelRGB8 128 32 224
-        , PixelRGB8 128 64 0
-        , PixelRGB8 128 64 32
-        , PixelRGB8 128 64 64
-        , PixelRGB8 128 64 96
-        , PixelRGB8 128 64 128
-        , PixelRGB8 128 64 160
-        , PixelRGB8 128 64 192
-        , PixelRGB8 128 64 224
-        , PixelRGB8 128 96 0
-        , PixelRGB8 128 96 32
-        , PixelRGB8 128 96 64
-        , PixelRGB8 128 96 96
-        , PixelRGB8 128 96 128
-        , PixelRGB8 128 96 160
-        , PixelRGB8 128 96 192
-        , PixelRGB8 128 96 224
-        , PixelRGB8 128 128 0
-        , PixelRGB8 128 128 32
-        , PixelRGB8 128 128 64
-        , PixelRGB8 128 128 96
-        , PixelRGB8 128 128 128
-        , PixelRGB8 128 128 160
-        , PixelRGB8 128 128 192
-        , PixelRGB8 128 128 224
-        , PixelRGB8 128 160 0
-        , PixelRGB8 128 160 32
-        , PixelRGB8 128 160 64
-        , PixelRGB8 128 160 96
-        , PixelRGB8 128 160 128
-        , PixelRGB8 128 160 160
-        , PixelRGB8 128 160 192
-        , PixelRGB8 128 160 224
-        , PixelRGB8 128 192 0
-        , PixelRGB8 128 192 32
-        , PixelRGB8 128 192 64
-        , PixelRGB8 128 192 96
-        , PixelRGB8 128 192 128
-        , PixelRGB8 128 192 160
-        , PixelRGB8 128 192 192
-        , PixelRGB8 128 192 224
-        , PixelRGB8 128 224 0
-        , PixelRGB8 128 224 32
-        , PixelRGB8 128 224 64
-        , PixelRGB8 128 224 96
-        , PixelRGB8 128 224 128
-        , PixelRGB8 128 224 160
-        , PixelRGB8 128 224 192
-        , PixelRGB8 128 224 224
-        , PixelRGB8 192 0 0
-        , PixelRGB8 192 0 32
-        , PixelRGB8 192 0 64
-        , PixelRGB8 192 0 96
-        , PixelRGB8 192 0 128
-        , PixelRGB8 192 0 160
-        , PixelRGB8 192 0 192
-        , PixelRGB8 192 0 224
-        , PixelRGB8 192 32 0
-        , PixelRGB8 192 32 32
-        , PixelRGB8 192 32 64
-        , PixelRGB8 192 32 96
-        , PixelRGB8 192 32 128
-        , PixelRGB8 192 32 160
-        , PixelRGB8 192 32 192
-        , PixelRGB8 192 32 224
-        , PixelRGB8 192 64 0
-        , PixelRGB8 192 64 32
-        , PixelRGB8 192 64 64
-        , PixelRGB8 192 64 96
-        , PixelRGB8 192 64 128
-        , PixelRGB8 192 64 160
-        , PixelRGB8 192 64 192
-        , PixelRGB8 192 64 224
-        , PixelRGB8 192 96 0
-        , PixelRGB8 192 96 32
-        , PixelRGB8 192 96 64
-        , PixelRGB8 192 96 96
-        , PixelRGB8 192 96 128
-        , PixelRGB8 192 96 160
-        , PixelRGB8 192 96 192
-        , PixelRGB8 192 96 224
-        , PixelRGB8 192 128 0
-        , PixelRGB8 192 128 32
-        , PixelRGB8 192 128 64
-        , PixelRGB8 192 128 96
-        , PixelRGB8 192 128 128
-        , PixelRGB8 192 128 160
-        , PixelRGB8 192 128 192
-        , PixelRGB8 192 128 224
-        , PixelRGB8 192 160 0
-        , PixelRGB8 192 160 32
-        , PixelRGB8 192 160 64
-        , PixelRGB8 192 160 96
-        , PixelRGB8 192 160 128
-        , PixelRGB8 192 160 160
-        , PixelRGB8 192 160 192
-        , PixelRGB8 192 160 224
-        , PixelRGB8 192 192 0
-        , PixelRGB8 192 192 32
-        , PixelRGB8 192 192 64
-        , PixelRGB8 192 192 96
-        , PixelRGB8 192 192 128
-        , PixelRGB8 192 192 160
-        , PixelRGB8 240 251 255
-        , PixelRGB8 164 160 160
-        , PixelRGB8 128 128 128
-        , PixelRGB8 0 0 255
-        , PixelRGB8 0 255 0
-        , PixelRGB8 0 255 255
-        , PixelRGB8 255 0 0
-        , PixelRGB8 255 0 255
-        , PixelRGB8 255 255 0
-        , PixelRGB8 255 255 255
-        ]
+    fillPixels'
+        :: MutableImage RealWorld PixelRGB8
+        -> VU.Vector Word8
+        -> [(Int, Int)]
+        -> IO ()
+    fillPixels' image imageData ((x, y) : coordinates) = do
+        let (pixel, imageData') = uncons imageData
+        writePixel image x y $ pallet ! fromIntegral pixel
+        fillPixels' image imageData' coordinates
+    fillPixels' _ _ [] = pure ()
+
+    uncons vec = let (v, vs) = VU.splitAt 1 vec in (VU.head v, vs)
 
 main :: IO ()
 main = convertFiles
